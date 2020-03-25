fa3f8f2fdcf13c88342542e0dbf45d5005a04132
Brian Fannin CAS <bfannin@casact.org>
2020-03-25 17:04:54 -0400

    Show development by days since crossed 100 case threshold

--- eda/ecdc.R
+++ eda/ecdc.R
@@ -7,3 +7,25 @@ tbl_ecdc %>%
   ggplot(aes(DateRep, cumulative_reported, color = country)) + 
   geom_line(show.legend = FALSE) + 
   theme_minimal()
+
+tbl_threshold <- tbl_ecdc %>% 
+  group_by(country) %>% 
+  filter(cumulative_reported >= 100) %>% 
+  arrange(DateRep, .by_group = TRUE) %>% 
+  mutate(
+    days_since = difftime(DateRep, head(DateRep, 1), units = "days") %>% as.numeric()
+  ) %>% 
+  ungroup()
+
+tbl_top_countries <- tbl_ecdc %>% 
+  group_by(country) %>% 
+  summarise(max_cumul = max(cumulative_reported)) %>% 
+  ungroup() %>% 
+  arrange(desc(max_cumul)) %>% 
+  head(10)
+
+tbl_threshold %>% 
+  inner_join(tbl_top_countries) %>% 
+  ggplot(aes(days_since, cumulative_reported, color = country)) + 
+  geom_line() + 
+  theme_minimal()
