17cf1a0efe2d3fc5132df5064fcd4eeb86b83c81
Brian Fannin CAS <bfannin@casact.org>
2020-03-26 11:55:48 -0400

    Add MPL2 license headers

--- data/ecdc.R
+++ data/ecdc.R
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 #' This script has been modified from original work found 
 #' at: https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide
 
